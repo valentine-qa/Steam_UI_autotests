69098e67b3fb6ef5908dac652374006e7d430615
valentine-qa <valentine.borodich13@gmail.com>
2025-06-25 17:14:24 +0300

    fixed conf for jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,97 @@
-import time
+import os
 
 import pytest
+
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 from selene import browser
+
+from steam_ui_autotests.utils import attach
+
+
+# @pytest.fixture(scope="session", autouse=True)
+# def load_env():
+#     load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_config():
-    browser.config.driver_name = 'chrome'
-    browser.config.window_width = '1800'
-    browser.config.window_height = '1000'
+def browser_management():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "122.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('user1')
+    password = os.getenv('1234')
+    driver = webdriver.Remote(
+        command_executor=f'https://user1:1234@selenoid.autotests.cloud/wd/hub',
+        options=options
+    )
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://store.steampowered.com'
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+
     yield
 
-    time.sleep(3)
+    attach.add_screenshot(browser)
+    attach.add_html(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+
     browser.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import time
+#
+# import pytest
+# from selene import browser
+#
+#
+# @pytest.fixture(scope='function', autouse=True)
+# def browser_config():
+#     browser.config.driver_name = 'chrome'
+#     browser.config.window_width = '1800'
+#     browser.config.window_height = '1000'
+#     browser.config.base_url = 'https://store.steampowered.com'
+#     yield
+#
+#     time.sleep(3)
+#     browser.quit()
