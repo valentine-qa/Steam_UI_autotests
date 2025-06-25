cb717503013d8a89b6cfff6626fa3d7f26d9a760
valentine-qa <valentine.borodich13@gmail.com>
2025-06-25 17:15:32 +0300

    fixed conf for jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,7 +19,7 @@ def browser_management():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "122.0",
+        "browserVersion": "127.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
